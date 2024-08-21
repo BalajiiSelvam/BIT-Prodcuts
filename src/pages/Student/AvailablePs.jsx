e43732570ef7a8adfaba91731e889389bf4c0555
BalajiiSelvam <balajiselvam245@gmail.com>
2024-08-21 18:17:32 +0530

    Sbar working

--- src/pages/Student/AvailablePs.jsx
+++ src/pages/Student/AvailablePs.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import StudentSideBar from '../../components/Ssidebar'
+import 'boxicons/css/boxicons.min.css';
+import './availableps.css'
 
 export default function AvailablePs() {
   return (
-    <>
-    <StudentSideBar/>
-    </>
+    <div className='availableps-container'>
+    <StudentSideBar />
+    <div className='content'>
+      <div className='top-box'>
+        <h2>&lt;&lt; AVAILABLE PROBLEM STATEMENTS &gt;&gt;</h2>
+      </div>
+    
+    </div>
+  </div>
    
   )
 }
